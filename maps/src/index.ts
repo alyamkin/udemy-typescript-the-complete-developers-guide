d27c1e6255e733d6a48fc596d6994f0fd8ee8877
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2023-04-30 17:53:41 -0400

    Add Section 9:Design Patterns with Typescript

---
+++ maps/src/index.ts
@@ -0,0 +1,9 @@
+/// <reference types="@types/google.maps" />
+
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+const map = new CustomMap('map');
+map.addMarker(new User());
+map.addMarker(new Company());
